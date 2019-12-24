c23ef52f226a772192a564ab07ee3d1e85c42ec2
Philip Winnington <ftcpjw@yahoo.com>
2019-12-23 20:53:12 -0500

    updating day1 python

--- 2019/python/day1/day1.py
+++ 2019/python/day1/day1.py
@@ -17,13 +17,9 @@ def main():
     total = 0
     for num in nums:
         fuel = calculate_fuel(num)
-        print(f"fuel calc after first call {fuel}")
         while fuel > 0:
-            print(f"in the while loop, total is {total}")
             total += fuel
-            print(f"added again {total}")
             fuel = calculate_fuel(fuel)
-            print(f"current fuel calc {fuel}")
     print(f"total for part 2 {total}")
 
 
